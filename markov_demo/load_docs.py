de6327b6fcb501a10f168b6648fd3b577d434317
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-02-14 11:56:14 -0800

    updates to the test documents; update the load_docs.py example to print citations as expected.

--- markov_demo/load_docs.py
+++ markov_demo/load_docs.py
@@ -20,14 +20,37 @@ for doc in [x for x in docs_json['documents']]:
         mm.add_document(contents, src, defer_recalc=True)
 mm.recalc_probabilities()
 
-print('**GENERATING 50 MARKOV CHAINS**')
+print('**GENERATING 10 MARKOV CHAINS**')
 for _ in range(0,10):
     start_tok = random.choice( list(mm.token_index_map.keys()) )
-    resp_dict = mm.get_markov_chain(500, start_tok)
+    resp_dict = mm.get_markov_chain(100, start_tok)
     resp_string = " ".join(resp_dict['markov_chain'])
 
+    sorted_citations = list()
     print(resp_string)
-    for x in resp_dict['sources']:
-        print(f'    {x}')
-    print()
+    for k, v in resp_dict['sources'].items():
+        sort_tuple = (v, k)
+        sorted_citations.append(sort_tuple)
+
+    sorted_citations = sorted(sorted_citations, reverse=True)
+    print("-> Top 3 Citations:")
+    for i in range(0,3):
+        if i >= len(sorted_citations):
+            print(f"    Only {i} sources for this chain!" )
+            break
+        k,v = sorted_citations[i] # unpack tuple to reverse key,value order
+        print(f'    {v}: {k} ')
+    #print(f'source: {k}, contribution count: {v}')
+
+#print(mm.transition_matx)
+#print(mm.token_index_map)
+#print(mm.index_token_map)
+#print(mm._counts)
+#for token in mm.token_index_map.keys():
+#print(mm.lookup_probs('and'))
+
+#nprint("transition matrix:")
+#print(mm.transition_matx)
+#print("lookup table")
+#print(mm.token_index_map)
 
