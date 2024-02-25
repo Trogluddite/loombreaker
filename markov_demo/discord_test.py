fee0fe19f125e0a4342d140a1e4bc2d7715721d9
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-02-25 01:21:22 -0800

    add new englishy text to discord bot

--- markov_demo/discord_test.py
+++ markov_demo/discord_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from random import choice
+from random import choice, randrange
 from string import punctuation
 
 import discord
@@ -36,32 +36,72 @@ class DiscordClient:
         # this really isn't searching -- we're just generating candidate text
         input_toks = [x.strip(punctuation) for x in incomming_message.split()]
         start_tok = choice(input_toks)
-        resp_dict = self.matMark.get_markov_chain(max_len=200, start_token=start_tok)
+        #fixme: should use an input tok
+        paragraph_data = self.get_paragraph()
 
         response_lines = list()
         response_lines.append("GENERATED FROM CORPUS:")
-        if len(resp_dict['markov_chain']) == 0:
+        if len(paragraph_data['sentences']) == 0:
             response_lines.append(
                 f"**token {start_tok} did not match any in the corpus :(**"
             )
             return "\n".join(response_lines)
         else:
             # response is python list with words in it
-            response_lines.append(f"{ ' '.join(resp_dict['markov_chain'])}")
+            response_lines.append(f"{ ' '.join(paragraph_data['sentences'])}")
 
         if show_sources:
-            sorted_citations = list()
-            for k,v in resp_dict['sources'].items():
-                sort_tuple = (v, k) # invert citation / count so we can sort by count
-                sorted_citations.append(sort_tuple)
-            sorted_citations = sorted(sorted_citations, reverse=True)
+            top_citations = self.dedupe_and_sort_citations(paragraph_data['citations'])[0:3]
             for i in range(0,3):
-                if i >= len(sorted_citations):
+                if i >= len(top_citations):
                     response_lines.append(f"    Only {i} sources for this chain!")
                     break
-                k,v = sorted_citations[i]
-                response_lines.append(f"    {v}: counted {k} times") #unpack sort tuple to reverse key/value
+                k,v = list(top_citations[i].items())[0]
+                response_lines.append(f"    {k}: counted {v} times")
+
         return "\n".join(response_lines)
+
+
+    def get_paragraph(self):
+        sentences = list()
+        unsorted_citations = list()
+
+        start_tok = choice( list(self.matMark.token_index_map.keys()) )
+
+        resp_dict = self.matMark.get_markov_chain(100, start_tok)
+        resp_dict['markov_chain'][0] = "    " + resp_dict['markov_chain'][0]
+        sentences.append(" ".join(resp_dict['markov_chain']))
+
+        unsorted_citations = unsorted_citations + list(dict(resp_dict['sources']).items())
+
+        for _ in range(0, randrange(5, 15)):
+            start_tok = choice( list(self.matMark.token_index_map.keys()) )
+            resp_dict = self.matMark.get_markov_chain(100, start_tok)
+            sentences.append(" ".join(resp_dict['markov_chain']))
+            unsorted_citations = unsorted_citations + list(dict(resp_dict['sources']).items())
+
+        resp = { 'sentences' : list(), 'citations' : list() }
+        resp['sentences'] = sentences
+        resp['citations'] = unsorted_citations
+        return resp
+
+    def dedupe_and_sort_citations(self, citation_list: list) -> list:
+        uniq_references = dict()
+
+        for ref in citation_list:
+            if uniq_references.get(ref[0], None):
+                uniq_references[ref[0]] = uniq_references[ref[0]] + ref[1]
+            else:
+                uniq_references[ref[0]] = ref[1]
+
+        sorted_citations = list()
+        for k, v in uniq_references.items():
+            sort_tuple = (v, k)
+            sorted_citations.append(sort_tuple)
+
+        sorted_citations = sorted(sorted_citations, reverse=True)
+        return [{v:k} for k, v in sorted_citations]
+
 
 def main():
     dc = DiscordClient()
