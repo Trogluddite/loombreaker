2a876607b0b72a9fd0a0dcbe56a124a2fc3e7133
KPilcher74 <156951217+KPilcher74@users.noreply.github.com>
2024-04-17 12:19:03 -0600

    Update home.page.ts

--- ionic/home.page.ts
+++ ionic/home.page.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
 
+interface SearchResult {
+  sentences?: string[];
+  text?: string;
+  citations?: {[key: string]: number}[];
+  top_citations?: {[key: string]: number}[];
+}
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  result?: SearchResult;
   query: string = '';
-  results: any[] = [];
+  clean: boolean = false;
+  showSources: boolean = false;
 
   constructor(private apiService: ApiService) {}
 
   search() {
-    this.apiService.search(this.query).subscribe(
+    this.apiService.search(this.query, this.clean, this.showSources).subscribe(
       data => {
-        this.results = data.sentences; // Assuming the response has a 'sentences' property
+        console.log("Data set to result:", data);
+        this.result = data;
       },
       error => {
         console.error('Error fetching data: ', error);
